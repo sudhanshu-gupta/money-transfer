1577934def79ce71c3656b5c61771a9c0f146443
sudhanshu.gupta <sudhanshu.gupta@careem.com>
2019-08-19 22:11:55 +0400

    adding reverse transaction

--- src/main/java/io/sudhanshugupta/moneytransfer/model/AccountRequest.java
+++ src/main/java/io/sudhanshugupta/moneytransfer/model/AccountRequest.java
@@ -2,6 +2,7 @@ package io.sudhanshugupta.moneytransfer.model;
 
 import java.math.BigDecimal;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import lombok.Value;
 
@@ -11,5 +12,6 @@ public class AccountRequest {
   private String name;
   @Email(message = "invalid email address")
   private String email;
+  @Min(value = 0, message = "initial balance must be greater or equal to zero")
   private BigDecimal balance;
 }
